8cd6fef18a5f6246f12957cf5333fd6870464b60
Cristian Maglie <c.maglie@bug.st>
2017-11-14 18:16:25 +0100

    Improved CardInfo example output
    Fix #12

--- examples/CardInfo/CardInfo.ino
+++ examples/CardInfo/CardInfo.ino
@@ -1,12 +1,12 @@
 /*
   SD card test
 
- This example shows how use the utility libraries on which the'
- SD library is based in order to get info about your SD card.
- Very useful for testing a card when you're not sure whether its working or not.
+  This example shows how use the utility libraries on which the'
+  SD library is based in order to get info about your SD card.
+  Very useful for testing a card when you're not sure whether its working or not.
 
- The circuit:
-  * SD card attached to SPI bus as follows:
+  The circuit:
+    SD card attached to SPI bus as follows:
  ** MOSI - pin 11 on Arduino Uno/Duemilanove/Diecimila
  ** MISO - pin 12 on Arduino Uno/Duemilanove/Diecimila
  ** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
@@ -14,11 +14,11 @@
  		Pin 4 used here for consistency with other Arduino examples
 
 
- created  28 Mar 2011
- by Limor Fried
- modified 9 Apr 2012
- by Tom Igoe
- */
+  created  28 Mar 2011
+  by Limor Fried
+  modified 9 Apr 2012
+  by Tom Igoe
+*/
 // include the SD library:
 #include <SPI.h>
 #include <SD.h>
@@ -58,7 +58,8 @@ void setup() {
   }
 
   // print the type of card
-  Serial.print("\nCard type: ");
+  Serial.println();
+  Serial.print("Card type:         ");
   switch (card.type()) {
     case SD_CARD_TYPE_SD1:
       Serial.println("SD1");
@@ -79,25 +80,30 @@ void setup() {
     return;
   }
 
+  Serial.print("Clusters:          ");
+  Serial.println(volume.clusterCount());
+  Serial.print("Blocks x Cluster:  ");
+  Serial.println(volume.blocksPerCluster());
+
+  Serial.print("Total Blocks:      ");
+  Serial.println(volume.blocksPerCluster() * volume.clusterCount());
+  Serial.println();
 
   // print the type and size of the first FAT-type volume
   uint32_t volumesize;
-  Serial.print("\nVolume type is FAT");
+  Serial.print("Volume type is:    FAT");
   Serial.println(volume.fatType(), DEC);
-  Serial.println();
 
   volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
   volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-  volumesize *= 512;                            // SD card blocks are always 512 bytes
-  Serial.print("Volume size (bytes): ");
+  volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
+  Serial.print("Volume size (Kb):  ");
   Serial.println(volumesize);
-  Serial.print("Volume size (Kbytes): ");
+  Serial.print("Volume size (Mb):  ");
   volumesize /= 1024;
   Serial.println(volumesize);
-  Serial.print("Volume size (Mbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-
+  Serial.print("Volume size (Gb):  ");
+  Serial.println((float)volumesize / 1024.0);
 
   Serial.println("\nFiles found on the card (name, date and size in bytes): ");
   root.openRoot(volume);
@@ -106,7 +112,5 @@ void setup() {
   root.ls(LS_R | LS_DATE | LS_SIZE);
 }
 
-
 void loop(void) {
-
 }
