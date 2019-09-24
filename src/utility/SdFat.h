6c9fa7f2b624dbe3bbf8c0441dcc3a8de90216a1
Martino Facchin <m.facchin@arduino.cc>
2019-09-24 10:13:18 +0200

    Undefine O_XXX macros if compiling for posix
    Fixes https://github.com/arduino/ArduinoCore-nRF528x-mbedos/issues/15

--- src/utility/SdFat.h
+++ src/utility/SdFat.h
@@ -40,6 +40,20 @@ class SdVolume;
 //==============================================================================
 // SdFile class
 
+#ifdef O_RDONLY //ARDUINO_ARCH_MBED
+#undef O_READ
+#undef O_RDONLY
+#undef O_WRITE
+#undef O_WRONLY
+#undef O_RDWR
+#undef O_ACCMODE
+#undef O_APPEND
+#undef O_SYNC
+#undef O_CREAT
+#undef O_EXCL
+#undef O_TRUNC
+#endif
+
 // flags for ls()
 /** ls() flag to print modify date */
 uint8_t const LS_DATE = 1;
