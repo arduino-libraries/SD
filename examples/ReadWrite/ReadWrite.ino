4d02d9bf2b82304f4a48201d3696cd5ab594c130
Deepak Khatri <deepaklorkhatri7@gmail.com>
2020-02-24 19:28:02 +0530

    Update ReadWrite.ino (#73) - Better debugging information when failing to init SD card
    * Update ReadWrite.ino
    * fix SdFat.h formatting
    * fix ReadWrite.ino formatting

--- examples/ReadWrite/ReadWrite.ino
+++ examples/ReadWrite/ReadWrite.ino
@@ -34,9 +34,14 @@ void setup() {
   Serial.print("Initializing SD card...");
 
   if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("1. is a card inserted?");
+    Serial.println("2. is your wiring correct?");
+    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
+    Serial.println("Note: press reset or reopen this serial monitor after fixing your issue!");
     while (1);
   }
+
   Serial.println("initialization done.");
 
   // open the file. note that only one file can be open at a time,
